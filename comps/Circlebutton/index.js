ccfc36d484c514c2f79b9b2ff7581b824816a555
maggiesuu <msu28@my.bcit.ca>
2021-04-04 00:18:20 -0700

    circle index.js

---
+++ comps/Circlebutton/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonInput = styled.button`
+    background-color:#FFEBEB;
+    width:80px;
+    height:80px;
+    border:none;
+    border-radius:100%;
+    box-shadow:0px 4px 4px rgba(0, 0, 0, 0.25);
+    font-family:'Athiti', sans-serif;
+    font-weight:600;
+    font-size:16px;
+    text-align:center;
+    word-wrap:break-word;
+    
+    
+`;
+
+const CircleButton = () => {
+    return <div>
+        <ButtonInput>
+            Costs Saved
+        </ButtonInput>
+    </div>
+}
+
+export default CircleButton;
