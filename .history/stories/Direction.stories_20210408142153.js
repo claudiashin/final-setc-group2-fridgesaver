cd7adc93dd718059c0ce75e7aad18b7540eac2a0
Leighai <72281788+Leighai@users.noreply.github.com>
2021-04-13 14:39:26 -0700

    Delete .history directory
    Deleting history folder.

--- .history/stories/Direction.stories_20210408142153.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Direction from '../comps/Directions';
-
-export default{
-    title:"Example/Directions",
-    component:<Direction />
-}
-export const DefaultDirection = () => <Direction />
--- .history/stories/Message.stories_20210408143444.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Card from '../comps/Message';
-
-export default{
-    title:"Example/Message",
-    component:<Message />
-}
-
-export const Message = () => <Message />
