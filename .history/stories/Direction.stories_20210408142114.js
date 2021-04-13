d4f48124a0c26d1b23895ff684e922ee32909b00
maggiesuu <msu28@my.bcit.ca>
2021-04-13 14:46:32 -0700

    dont know what it is

--- .history/stories/Direction.stories_20210408142114.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Direction from '../comps/Directions';
-
-export default{
-    title:"Example/Direction",
-    component:<Direction />
-}
-export const DefaultDirection = () => <Direction />
