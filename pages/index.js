c863765e5443a637e4e59bfab47650c48234bcfc
maggiesuu <msu28@my.bcit.ca>
2021-04-13 23:29:14 -0700

    index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import Line from '../comps/Line';
 import Startbutton from '../comps/Startbutton';
 import Information from '../comps/Information';
 import styled from 'styled-components';
+import {Router, useRouter} from 'next/router';
 
 
 const HomeCont = styled.div`
-max-width: 375px;
-height: 812px;
+max-width: 400px;
+height: 900px;
 background-color: #FFF8F8;
 
 .navigationbar {}
@@ -24,20 +25,35 @@ background-color: #FFF8F8;
     width: 200px;
 }
 
-.cta-fridge 
-  img{
-    width: 100px;
+.cta-fridge {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 50px;
 }
+
+.arrow {
+  width: 80px;
+}
+
+.fridge {
+    width: 120px;
+}
+
 
 .information {
   margin: 25px;
 }
 
-.start-button {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 40px;
+// .start-button {
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   margin: 40px;
+// }
+
+.footer {
+  margin: 10px;
 }
 `;
 
@@ -51,19 +67,21 @@ export default function Home() {
     </div>
 
     <div className="cta-fridge">
-      <img src="fridge.png"></img>
+      <img className="arrow" src="right-arrow.svg"></img>
+      <img className="fridge" src="fridge.png" onClick={()=>Router.push("/landingpagetwo")}></img>
     </div>
 
     <div className="information">
       <Information titletext="WHAT THIS APP DOES" paratext="This mobile application provides recipes focused on using the foods in your fridge to save time and costs, all while minimizing food waste."/>
     </div>
 
-    <div className="start-button">
+    {/* <div className="start-button">
       <Startbutton text="Let's Get Started"/>
-    </div>
+    </div> */}
+
+    <Line/>
 
     <div className="footer">
-      <Line/>
       <Developer name1="Iori Takeshita" name2="Claudia Shin" name3="Maggie Su" name4="Leighai Nishibata"/>
     </div>
   
