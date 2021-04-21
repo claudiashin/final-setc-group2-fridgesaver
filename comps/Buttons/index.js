474e67ad49b8cde7195655f7697da807690f5188
Iori Takeshita <takeshitaiori@gmail.com>
2021-04-22 01:21:37 +0900

    Changes Footer, dish comp, Button comp, recipe

--- comps/Buttons/index.js
+++ comps/Buttons/index.js
@@ -3,37 +3,38 @@ import styled from 'styled-components';
 import {useRouter} from "next/router"
 
 const ButtonCont = styled.span`
-margin:5px;
+    margin: 5px;
 `;
-
-// const ButtonImg =styled.img`
-// width:5px,
-// height:auto,
-// object-fit:contain`
-
 
 const ButtonInput = styled.button`
-      background-color:${props => props.bgcolor};
-      color:#FFF;
-      padding:10px;
-      border-radius:5px;
-      border:none;
+background-color: ${props=>props.bgcolor};
+width: 240px;
+padding: 25px;
+border-radius: 5px;
+margin-bottom:10px;
+border: none;
+display: flex;
+justify-content: center;
+align-items: center;
+font-family: 'Athiti', sans-serif;
+font-weight: bold;
+font-size: 18px;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
-const Button = ({text = "Test Buttons",
-bgcolor = '#AFCF6B',
+const Button = ({text = "Topic1",
+bgcolor = '#FEF2CA',
 routeTo = "/",
 onClick=()=>{}}) => {
 
 const rounter  = useRouter()
 // ()=> rounter.push(routeTo)
     return (<ButtonCont onClick = {onClick}>
-        {/* <button style= {{backgroundColor:"#AFCF6B",
-        color:"#fff",}}>Test Button</button> */}
+
             <ButtonInput bgcolor ={bgcolor}>
              {text}
             </ButtonInput>
-            {/* <ButtonImg src = "/vercel.svg"></ButtonImg> */}
+
          </ButtonCont>
 
      )
--- comps/Footer/index.js
+++ comps/Footer/index.js
@@ -1,35 +1,76 @@
 import React from 'react';
 import styled from 'styled-components';
-import {ImHome} from "react-icons/im";
+import Link from 'next/link';
+
 
 const FooterCont  = styled.div`
 display:flex;
 flex-direction:row;
-width: 100%;
-height: 60px;
-background-color: #9FCDA4;
+width:100%;
+height:20%;
+margin:0px;
+ 
+.IconCont{
+       display:flex;
+       align-items:center;
+       justify-content:center;
+       width:80px;
+       height:80%;  
+       margin:10px;
+}   
+
+
 `
 
 const Iconbox = styled.div`
-display:flex;
-width: 30%;
-height: 60px;
-border-radius:20px;
+ display:flex;
+ align-items:center;
+ justify-content:center;
+ width: 100%;
+ height: 50%;
+ background-color: #9FCDA4;   
 `
 
+const IconImage = styled.img`
+width: 40%;
+height: 100%;
 
 
-const  Footer = () => {
+
+`
+const  Footer = ({
+    
+}) => {
     return( 
     
-      <FooterCont>
-
+    <FooterCont>
         <Iconbox>
-            <div><ImHome/></div>
-        </Iconbox>    
+              <div className = "IconCont">
+                <Link href = "/">
+                    <IconImage src = "/home.png"/>
+                </Link>
+              </div> 
 
+              <div className = "IconCont">
+                <Link href = "/saved">  
+                  <IconImage src ="/share.png" />
+                </Link>    
+              </div>
 
-        </FooterCont>
+              <div className = "IconCont">
+                <Link href = "/saved">  
+                    <IconImage src ="/bookmarks.png"/>
+                </Link>
+              </div>
+
+          <div className = "IconCont">
+            <Link href = "/about">  
+              <IconImage src ="/planet.png"/>
+            </Link>  
+          </div>
+        </Iconbox> 
+   
+    </FooterCont>
 
     )
 }
