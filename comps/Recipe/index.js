9ca95600b62acdd8c5377c648aa8311e1eb73a9e
LeighaiNishibata <vnishibata@my.bcit.ca>
2021-05-14 22:52:37 -0700

    added results pages & changed recipe page

--- comps/Recipe/index.js
+++ comps/Recipe/index.js
@@ -9,6 +9,7 @@ const RecipeCont = styled.div`
     justify-content: center;
     align-items: center;
     margin-top: 30px;
+    margin-bottom: 15px;
     
     @media only screen and (max-width: 400px){
         display: flex;
@@ -16,27 +17,24 @@ const RecipeCont = styled.div`
         flex-wrap: wrap;
     }
 `;
+
 const ImageDiv = styled.div`
     width: 150px;
     height: 150px;
     margin-right: 5px;
 `;
 
-
 const RecipeImage = styled.img`
     width: 100%;
     height: 100%; 
     object-fit: cover;
-
 `;
 
 const IngredientsCont = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: flex-start;
-    width: 220px;
-    height: 200px; 
+    align-items:center;
     margin: 15px 0px 15px 10px;
 `;
 
@@ -46,24 +44,9 @@ const RecipeTitle = styled.h2`
     margin: 0px;
 `;
 
-const IngredientsList = styled.ul`
-    font-family: 'Athiti', sans-serif;
-    list-style-type: none;
-    line-height: 1.3em;
-    text-align: left;
-    padding: 0px;
-    margin-top: 10px;
-`;
-
 const Recipe = ({
     image="/DN-Onion.jpg",
     text="Onion Dry Noodle",
-    listText1="1 pkg (8 ounce) egg noodles",
-    listText2="3 tablespoons butter",
-    listText3="½ pound bacon",
-    listText4="1 onion, chopped",
-    listText5="1 small head cabbage, chopped",
-    listText6="1 dash garlic salt"
 
 }) => {
     return <RecipeCont>
@@ -72,14 +55,6 @@ const Recipe = ({
         </ImageDiv>
         <IngredientsCont>
             <RecipeTitle>{text}</RecipeTitle>
-            <IngredientsList>
-                <li>{listText1}</li>
-                <li>{listText2}</li>
-                <li>{listText3}</li>
-                <li>{listText4}</li>
-                <li>{listText5}</li>
-                <li>{listText6}</li>  
-            </IngredientsList>
         </IngredientsCont>
     </RecipeCont>
 }
